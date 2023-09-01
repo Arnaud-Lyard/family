191854de23b712d162ec8b7971c62b236645a1cb
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-09-01 14:30:51 +0200

    ✨ install tiptap and prepare form input

--- client/src/pages/Admin.vue
+++ client/src/pages/Admin.vue
@@ -1,5 +1,23 @@
 <template>
-    <div class="container flex flex-center">
-        Admin
+  <div class="container flex flex-justify-center">
+    <div class="editor-bloc">
+      <Editor v-model="content" />
+      <button class="button button-center" :class="drawerActive" @click="test()">Save</button>
     </div>
+    <ArticleAdmin />
+  </div>
 </template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import Editor from "../components/Editor.vue"
+import ArticleAdmin from "../components/ArticleAdmin.vue"
+import { useDrawerActive } from "../composables/drawerActive";
+const content = ref("")
+
+const test = () => {
+  console.log(content.value)
+}
+
+const { drawerActive } = useDrawerActive();
+
+</script>
