42d280ed0e60adf84824dd925c8d04443661a9a0
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-10-14 17:12:16 +0200

    insert logo and adapt height

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav>
       <ul class="navigation">
         <li>
-          <img src="/images/vue-js.png" alt="Logo">
+          <img src="/images/relaxing-hippoquests.jpeg" alt="Logo">
         </li>
         <li>
           <RouterLink :to="{ name: 'home' }">Home</RouterLink>
